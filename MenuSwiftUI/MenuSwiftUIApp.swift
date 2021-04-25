085e337a8bafd1ff47cc891ce6e903ac9c27e404
Jesus Mora <jesusmora0101@gmail.com>
2021-04-25 17:58:17 -0500

    Initial Commit

---
+++ MenuSwiftUI/MenuSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuSwiftUIApp.swift
+//  MenuSwiftUI
+//
+//  Created by Data on 25/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MenuSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
